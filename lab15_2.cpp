7ff852e213bde8953ca037c1c7b2258ae0a901f7
Chayathon <chayathon_p@cmu.ac.th>
2023-01-17 17:17:06 +0700

    lab15_2 COMPLETE

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,48 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double matrix[N][N])
+{
+    for (int i = 0; i < N; i++) 
+	  {
+		cout << "Row " << i+1 << ": ";
+        for (int j = 0; j < N; j++) 
+		{
+            cin >> matrix[i][j];
+        }
+    }
+}
+void findLocalMax(const double matrix[N][N], bool B[N][N])
+{
+   for (int i = 0; i < N; i++) 
+   {
+        for (int j = 0; j < N; j++) {
+            if (i > 0 && i < N - 1 && j > 0 && j < N - 1) 
+            {
+                if (matrix[i][j] >= matrix[i - 1][j] && matrix[i][j] >= matrix[i + 1][j] && matrix[i][j] >= matrix[i][j - 1] && matrix[i][j] >= matrix[i][j + 1]) 
+                {
+                    B[i][j] = true;
+                } 
+                else 
+                {
+                    B[i][j] = false;
+                }
+            } 
+            else 
+            {
+                B[i][j] = false;
+            }
+        }
+    }
+}
+void showMatrix(const bool matrix[N][N])
+{
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+}

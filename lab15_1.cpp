d6106c05390eabb479b074a74bdb45e08c426d25
Chayathon <chayathon_p@cmu.ac.th>
2023-01-17 17:16:45 +0700

    lab15_1 COMPLETE

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	  int  j;
+    for (int i = 1; i < N; i++) {
+        
+        j = i ;
+        while (j > 0 && d[j-1] < d[j]) {
+            T temp = d[j] ;
+            d[j] = d[j-1];
+            d[j-1] = temp;
+            j--;
+        }
+       
+        cout << "Pass " << i << ":";
+        for (int k = 0; k < N; k++) {
+            cout << d[k] << " ";
+        }
+        cout << endl;
+    }
 
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
